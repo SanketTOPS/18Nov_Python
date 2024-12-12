3a386d8dc4b5125d8df788cf363c2cd431858e32
SanketTops <sanket.tops@gmail.com>
2024-12-12 09:48:37 +0530

    Looping

---
+++ Module-1/floting_val.py
@@ -0,0 +1,9 @@
+a=5.27
+b=45.26
+
+#print("Sum:",a+b)
+c=a+b
+#print("Sum:",c)
+#print(f"Sum:{c:.2f}")
+print(f"{c:.2f}")
+
---
+++ Module-1/while_1.py
@@ -0,0 +1,12 @@
+"""i=1
+
+while i<=10: #TRUE
+    print(i) #statement
+    i+=1 #i=i+1"""
+
+
+i=1
+
+while i<=10:
+    print("Sanket")
+    i+=1
