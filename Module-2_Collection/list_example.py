11936a7b512e5f2c08ab0a4694476f051928829c
SanketTops <sanket.tops@gmail.com>
2024-12-19 09:36:43 +0530

    List

---
+++ Module-2_Collection/list_example.py
@@ -0,0 +1,56 @@
+city=['rajkot','surat','baroda','gandhinagar','morbi']
+"""print(city)
+print(city[0])
+print(city[-1])
+print(city[0:3]) #range
+print(city[:4])
+print(city[2:])"""
+
+# ------------------------------------------ #
+#print(city)
+#city[2]='ahmedabad' #change value
+
+#print(city)
+#print(len(city))
+
+"""if 'surat' in city:
+    print("Yes...")
+else:
+    print("No")"""
+
+# ------------------------------- #
+#print(city)
+
+"""for i in city:
+    print(i)
+    print(city.index(i))"""
+
+"""for i in city:
+    print(f"{city.index(i)} {i}")"""
+
+
+# ------------------ List Methods ------------------- #
+print(city)
+#city.append("jamnagar")
+#city.insert(3,'bhavnagar')
+#city.remove('morbi')
+#city.pop()
+#city.pop(1)
+#city.clear()
+#city.sort()
+#city.reverse()
+#del city[0]
+#del city
+#print(city)
+
+#newlist=city.copy()
+#print(newlist)
+
+newlist=['A','B','C','D']
+print(newlist)
+#city.extend(newlist)
+#print(city)
+print(city+newlist)
+
+
+
